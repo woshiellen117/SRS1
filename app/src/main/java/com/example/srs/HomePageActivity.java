fff8fbd8b3f49b45a2f5fa0497e120fb9928e199
woshiellen117 <woshiellen2@163.com>
2019-02-12 20:57:02 -0600

    aa

---
+++ app/src/main/java/com/example/srs/HomePageActivity.java
@@ -0,0 +1,24 @@
+package com.example.srs;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.srs.activity.MainActivity;
+
+public class HomePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView2);
+        textView.setText(message);
+    }
+}
