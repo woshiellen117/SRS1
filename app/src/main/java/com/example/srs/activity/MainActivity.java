a6c3ce1c49370588ecf7a9f20e6dfedbd2a12973
woshiellen117 <woshiellen2@163.com>
2019-02-14 11:28:18 -0600

    sprint1

--- app/src/main/java/com/example/srs/activity/MainActivity.java
+++ app/src/main/java/com/example/srs/activity/MainActivity.java
@@ -1,179 +1,90 @@
 package com.example.srs.activity;
 
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.example.srs.R;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.Cursor;
 import com.example.srs.helper.*;
-
+import com.example.srs.R;
 
 public class MainActivity extends AppCompatActivity {
-    private DBHelper dbHelper;
 
-    public static final String EXTRA_MESSAGE = "com.example.srs.MESSAGE";
-    public static int register_answer_question=0;
+
+    private DBHelper dbHelper;
+    private Button btn;
+    private EditText EdtUser;
+    private EditText EdtPwd;
+    private TextView forget;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        dbHelper = new DBHelper(this, "stock.db", null, 1);
-        //创建db
-        final SQLiteDatabase db = dbHelper.getWritableDatabase();
-
-//        final Spinner spinner = (Spinner) findViewById(R.id.spinner);
-
-
-//        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            @Override
-//            public void onItemSelected(AdapterView<?> parent, View view,
-//                                       int pos, long id) {
-//
-//                String[] questions = getResources().getStringArray(R.array.a);
-////                String register_answer_question;
-//                register_answer_question =pos;
-//            }
-//            @Override
-//            public void onNothingSelected(AdapterView<?> parent) {
-//                // Another interface callback
-//            }
-//        });
-
-        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
-
+        btn=(Button) findViewById(R.id.button);
+        EdtUser=findViewById(R.id.editText);
+        EdtPwd=findViewById(R.id.editText2);
+        btn.setOnClickListener(new BtnClickLinstener());
+        forget=(TextView) findViewById(R.id.textView);
+        forget.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-                refresh();
-
-//                EditText editText = findViewById(R.id.editText13);
-//                String register_age = editText.getText().toString();
-//
-//
-//                editText = findViewById(R.id.editText2);
-//                String register_name = editText.getText().toString();
-//
-//                editText = findViewById(R.id.editText3);
-//                String register_username = editText.getText().toString();
-//
-//                editText = findViewById(R.id.editText6);
-//                String register_gender = editText.getText().toString();
-//
-//                editText = findViewById(R.id.editText7);
-//                String register_phone_num = editText.getText().toString();
-//
-//
-//                editText = findViewById(R.id.editText9);
-//                String register_answer = editText.getText().toString();
-//
-//                editText = findViewById(R.id.editText10);
-//                String register_password = editText.getText().toString();
-//
-//                editText = findViewById(R.id.editText12);
-//                String register_confirm_password = editText.getText().toString();
-//
-//
-//                TextView showtext = findViewById(R.id.textView4);
-//                if(alreadyexists(register_username))
-//                {
-//                    showtext.setText("The username already exists!\n");
-//                }
-//                else if(!register_confirm_password.equals(register_password))
-//                {
-//                    showtext.setText("The two passwords not match!\n");
-//                }
-//                else if(register_age.equals("")||register_name.equals("")||register_username.equals("")||register_gender.equals("")||register_phone_num.equals("")||register_password.equals("")||register_confirm_password.equals("")||register_answer.equals(""))
-//                {
-//                    showtext.setText("Please enter all blank fields!\n");
-//                }
-//                else if(register_password.length()<8)
-//                {
-//                    showtext.setText("The length of password should be more than 8 character!\n");
-//                }
-//                else
-//                {
-//                    int flag_int=0;
-//                    int flag_char=0;
-//                    for(int i=0;i<register_password.length();i++)
-//                    {
-//                        if(register_password.charAt(i)<'9'&&register_password.charAt(i)>'0')
-//                        {
-//                            flag_int=1;
-//                        }
-//                        else if(register_password.charAt(i)<'z'&&register_password.charAt(i)>'a'||register_password.charAt(i)<'Z'&&register_password.charAt(i)>'A')
-//                        {
-//                            flag_char=1;
-//                        }
-//                    }
-//                    if(!(flag_char==1&&flag_int==1))
-//                    {
-//                        showtext.setText("Password should contain both digits and characters!\n");
-//                    }
-//                    else
-//                    {
-//                        db.execSQL(getString(R.string.insertData), new String[]{register_username, register_password, register_name,register_gender, register_age, register_phone_num,Integer.toString(register_answer_question),register_answer});
-//                        showtext.setText("db\n");
-//                        Cursor cursor = db.query("user", null, null, null, null, null, null);
-//                        if (cursor.moveToFirst()) {
-//                            do {
-//                                //遍历Cursor对象
-//                                String username = cursor.getString(cursor.getColumnIndex("username"));
-//                                String password = cursor.getString(cursor.getColumnIndex("password"));
-////                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
-////                        double price = cursor.getDouble(cursor.getColumnIndex("price"));
-//
-//                                String result = "The user is" + username + "and password is " + password;
-//                                Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
-//                            } while (cursor.moveToNext());
-//                        }
-//                        cursor.close();
-//
-//                    }
-//
-//                }
-
-//                refresh();
+                Intent intents = new Intent(MainActivity.this, ForgetActivity.class);
+                startActivity(intents);
             }
         });
 
+        Button reg=(Button) findViewById(R.id.button2);
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                refresh();
+            }
+        });
+        dbHelper=new DBHelper(this,"stock.db",null,1);
+
     }
 
-    /** Called when the user taps the Send button */
-    public void sendMessage(View view) {
-        // Do something in response to button
+
+    class BtnClickLinstener implements View.OnClickListener{
 
 
-        EditText editText = findViewById(R.id.editText10);
-        String register_password = editText.getText().toString();
+        public void onClick(View v){
+            String inputname=EdtUser.getText().toString();
+            String inputpwd=EdtPwd.getText().toString();
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
+            Cursor cursor = db.rawQuery("select password from user where username=?",new String[]{inputname});
+            if (cursor.moveToFirst()) {
+                //遍历Cursor对象
+                String password = cursor.getString(cursor.getColumnIndex("password"));
+//                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+//                        double price = cursor.getDouble(cursor.getColumnIndex("price"));
+                if (password.equals(inputpwd)){
+                    Toast.makeText(getApplicationContext(), "login correct", Toast.LENGTH_SHORT).show();
+                    //Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                    //                startActivity(intent);
+                    Intent intents = new Intent(MainActivity.this, HomepageActivity.class);
+                    startActivity(intents);
 
-        editText = findViewById(R.id.editText12);
-        String register_confirm_password = editText.getText().toString();
-
-        if(!register_confirm_password.equals(register_password))
-        {
-            Intent intent = new Intent(this, MainActivity.class);
-            TextView textView = findViewById(R.id.textView4);
-            String message = textView.getText().toString();
-            intent.putExtra(EXTRA_MESSAGE, message);
-            startActivity(intent);
+                }else{
+                    Toast.makeText(getApplicationContext(), "username/password is incorrect", Toast.LENGTH_SHORT).show();
+                }
+                //String result = "The user is" + username + "and password is " + password;
+                //Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(getApplicationContext(), "username/password is incorrect", Toast.LENGTH_SHORT).show();
+            }
+            cursor.close();
         }
-
-//        TextView editText = findViewById(R.id.textView4);
-//        String message = editText.getText().toString();
-//        intent.putExtra(EXTRA_MESSAGE, message);
-//        startActivity(intent);
     }
 
-    /**
+
+
+        /**
      * 刷新
      */
     private void refresh() {
@@ -182,25 +93,5 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public boolean alreadyexists(String username)
-    {
-//        dbHelper = new DBHelper(this, "stock.db", null, 1);
-//        //创建db
-        final SQLiteDatabase db = dbHelper.getWritableDatabase();
-        Cursor cursor = db.query("user", null, null, null, null, null, null);
-
-        if (cursor.moveToFirst()) {
-            do {
-                //遍历Cursor对象
-                String findusername = cursor.getString(cursor.getColumnIndex("username"));
-                String result = "The user is " + findusername ;
-                Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
-                if(findusername.equals(username))
-                    return true;
-            } while (cursor.moveToNext());
-        }
-        cursor.close();
-        return false;
-    }
 
 }
